cf99b0da1ca38cffef0f5347bf5ea11c4d68add8
Bayron <Ponce.bayron.bayron944@gmail.com>
2018-02-07 13:14:07 -0600

    Proyecto

---
+++ ProyectoBela/Bela.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bela.UI.Startup))]
+namespace Bela.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
