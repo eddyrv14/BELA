6ffa76047aa7f7f97c5a5e45338b6b839266ca8a
Bayron <Ponce.bayron.bayron944@gmail.com>
2018-02-28 18:56:14 -0600

    Cambios

---
+++ ProyectoBela/Bela.UI/Controllers/NoticiaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bela.UI.Controllers
+{
+    public class NoticiaController : Controller
+    {
+        //
+        // GET: /Noticia/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
