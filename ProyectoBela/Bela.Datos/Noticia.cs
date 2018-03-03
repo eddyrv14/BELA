9bfbd5c4f3a4c5e13f59012f10fcf38139275994
Bayron <Ponce.bayron.bayron944@gmail.com>
2018-03-03 17:17:02 -0600

    Datos-Cambios clase Noticias
    Cambio en atributos

--- ProyectoBela/Bela.Datos/Noticia.cs
+++ ProyectoBela/Bela.Datos/Noticia.cs
@@ -8,8 +8,10 @@ namespace Bela.Datos
 {
    public class Noticia
     {
-       public int idNoticias { get; set; }
-       public string titulo { get; set; }
-       public string descripcion { get; set; }
+        public int idNoticia { get; set; }
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+        public string imagen { get; set; }
+        public DateTime fechaYHora { get; set; }
     }
 }
