8f34d8f26a39f91f80f00f799fbe5301773f5332
Bayron <Ponce.bayron.bayron944@gmail.com>
2018-03-07 19:47:14 -0600

    Cambios pagina login

---
+++ ProyectoBela/Bela.UI/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bela.UI.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //
+        // GET: /Usuario/
+        public ActionResult Login()
+        {
+            return View();
+        }
+	}
+}
