f37686d0386579d25fb16543fe097bcb5caf7eea
Bayron <Ponce.bayron.bayron944@gmail.com>
2018-04-11 19:01:49 -0600

    Vista Inicio Profesor y crear Material

--- ProyectoBela/Bela.UI/Controllers/ProfesorController.cs
+++ ProyectoBela/Bela.UI/Controllers/ProfesorController.cs
@@ -17,5 +17,10 @@ namespace Bela.UI.Controllers
             }
             return View();
         }
+
+        public ActionResult CrearMaterial()
+        {
+            return View();
+        }
     }
 }
