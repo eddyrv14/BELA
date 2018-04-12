3c69de7b22fe6feba0822b3a406adb1cdee42f05
Bayron <Ponce.bayron.bayron944@gmail.com>
2018-04-11 19:24:46 -0600

    Clases e interfaces para las actividades de profesor

---
+++ ProyectoBela/Bela.BL/Metodos/MProfesorBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bela.BL.Interfaces;
+using Bela.Datos;
+
+namespace Bela.BL.Metodos
+{
+    public class MProfesorBL : IProfesor
+    {
+        public string crearMaterial(DetalleMateria materia)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProyectoBela/Bela.Datos/DetalleMateria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bela.Datos
+{
+   public class DetalleMateria
+    {
+        public int idMaterial { get; set; }
+        public int idUsuario { get; set; }
+        public int idDetalleMateria { get; set; }
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+        public string material { get; set; }
+        public DateTime fechaHora { get; set; }
+    }
+}
